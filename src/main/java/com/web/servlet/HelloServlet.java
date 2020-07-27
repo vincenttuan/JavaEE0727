5588c1737f1126b636ef38eb070af87d4c67f09f
teacher <teacher@192.168.2.14>
2020-07-27 21:37:55 +0800

    JavaWeb

---
+++ src/main/java/com/web/servlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("Hello Servlet:" + new Date());
+    }
+    
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,17 @@
+<%@page import="java.util.Date"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>
+            <%
+                Date today = new Date();
+                out.print(today);
+            %>
+        </h1>
+    </body>
+</html>
