807370cc4848ecb3687c8eb5c6e80962314656a1
teacher <teacher@192.168.2.14>
2020-07-31 21:14:17 +0800

    JavaWeb

--- src/main/java/com/web/servlet/BMIServlet.java
+++ src/main/java/com/web/servlet/BMIServlet.java
@@ -8,13 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class BMIServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    
+    private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String h = req.getParameter("h");
         String w = req.getParameter("w");
         PrintWriter out = resp.getWriter();
-        out.print("doGet()... ");
         if(h == null || w == null) {
             out.print("Please input h, w !");
             return;
@@ -24,21 +22,18 @@ public class BMIServlet extends HttpServlet {
         double bmiValue = weight / Math.pow(height/100, 2);
         out.print(String.format("%.2f", bmiValue));
     }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print("doGet not working... ");
+        out.print("<a href='/JavaWeb0727/form/bmiform.jsp'>Click me</a>");
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String h = req.getParameter("h");
-        String w = req.getParameter("w");
-        PrintWriter out = resp.getWriter();
-        out.print("doPost()... ");
-        if(h == null || w == null) {
-            out.print("Please input h, w !");
-            return;
-        }
-        double height = Double.parseDouble(h);
-        double weight = Double.parseDouble(w);
-        double bmiValue = weight / Math.pow(height/100, 2);
-        out.print(String.format("%.2f", bmiValue));
+        doHandle(req, resp);
     }
 
     
