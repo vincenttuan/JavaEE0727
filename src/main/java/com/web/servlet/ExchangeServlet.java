2819da13f748510cb04726fef5d55be93fa48da8
teacher <teacher@192.168.2.14>
2020-08-03 20:45:02 +0800

    JavaWeb

---
+++ src/main/java/com/web/servlet/ExchangeServlet.java
@@ -0,0 +1,16 @@
+package com.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExchangeServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+    
+}
