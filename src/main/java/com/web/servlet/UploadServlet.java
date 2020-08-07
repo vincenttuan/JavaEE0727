71ab4e268bc45cd85a7a209b021f31c26a1f8ee9
teacher <teacher@192.168.2.14>
2020-08-07 20:50:19 +0800

    JavaWeb

---
+++ src/main/java/com/web/servlet/UploadServlet.java
@@ -0,0 +1,29 @@
+package com.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/servlet/upload"})
+@MultipartConfig(
+        fileSizeThreshold = 1024*1024*1,
+        maxFileSize = 1024*1024*10,
+        maxRequestSize = 1024*1024*20,
+        location = "C:/upload"
+)
+public class UploadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        
+        
+    }
+    
+}
