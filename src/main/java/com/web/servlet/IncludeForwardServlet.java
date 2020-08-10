7911152a8e8c5f02bd6946cce8019acc77f961ad
teacher <teacher@192.168.2.14>
2020-08-10 19:20:06 +0800

    JavaWeb

--- src/main/java/com/web/servlet/IncludeForwardServlet.java
+++ src/main/java/com/web/servlet/IncludeForwardServlet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/servlet/includeAndForward")
 public class IncludeForwardServlet extends HttpServlet {
 
     @Override
@@ -17,7 +19,10 @@ public class IncludeForwardServlet extends HttpServlet {
         int y = 20;
         String path = "/result/add.jsp";
         RequestDispatcher rd = getServletContext().getRequestDispatcher(path);
-        rd.forward(req, resp);
+        req.setAttribute("x", x);
+        req.setAttribute("y", y);
+        //rd.forward(req, resp);
+        rd.include(req, resp);
         
         PrintWriter out = resp.getWriter();
         out.print("Finish");
