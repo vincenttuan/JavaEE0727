27ab35c7601a0e7d3ee664608903e3d2e267c089
teacher <teacher@192.168.2.14>
2020-08-10 21:08:30 +0800

    JavaWeb

---
+++ src/main/java/com/web/filter/MyCarFilter.java
@@ -0,0 +1,21 @@
+package com.web.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/secure/mycar/*")
+public class MyCarFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        PrintWriter out = res.getWriter();
+        out.print("GG");
+    }
+    
+}
