5a443e888626093c80b3028ac648b19dd61a51d5
teacher <teacher@192.168.2.14>
2020-08-10 21:28:53 +0800

    JavaWeb

--- src/main/java/com/web/filter/MyCarFilter.java
+++ src/main/java/com/web/filter/MyCarFilter.java
@@ -3,6 +3,7 @@ package com.web.filter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
@@ -14,16 +15,17 @@ public class MyCarFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        //int age = Integer.parseInt(req.getParameter("age"));
         String age = req.getParameter("age");
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/form/mycarform.jsp");
         try {
             if(age == null || Integer.parseInt(age) < 18) {
-                res.sendRedirect("/JavaWeb0727/form/mycarform.jsp");
+                //res.sendRedirect("/JavaWeb0727/form/mycarform.jsp");
+                rd.forward(req, res);
             } else {
                 chain.doFilter(req, res);
             }
         } catch (Exception e) {
-            res.sendRedirect("/JavaWeb0727/form/mycarform.jsp");
+            rd.forward(req, res);
         }
         
     }
