857b89cd2ab4acc11ff64cbccd968bdf8edc6e10
teacher <teacher@192.168.2.14>
2020-08-14 20:00:02 +0800

    JavaWeb

--- src/main/java/com/clockon/controller/ClockOnReportServlet.java
+++ src/main/java/com/clockon/controller/ClockOnReportServlet.java
@@ -2,24 +2,29 @@ package com.clockon.controller;
 
 import com.clockon.model.ClockOnModel;
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/controller/clockon")
-public class ClockOnServlet extends HttpServlet {
+@WebServlet("/controller/clockonreport")
+public class ClockOnReportServlet extends HttpServlet{
     private ClockOnModel model = new ClockOnModel();
+    protected void doHandler(String no, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print(model.queryClockOn(no));
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String image = req.getParameter("image");
+        String no = req.getAttribute("no") + "";
+        doHandler(no, req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String no = req.getParameter("no");
-        boolean check = model.clockOn(no, image);
-        RequestDispatcher rd = getServletContext().getRequestDispatcher("/controller/clockonreport");
-        req.setAttribute("no", no);
-        rd.forward(req, resp);
+        doHandler(no, req, resp);
     }
     
 }
