1e0f00d1d9c0eb8db2857485fe7fde91ca2be5e0
teacher <teacher@192.168.2.14>
2020-08-17 19:27:45 +0800

    JavaWeb

--- src/main/java/com/web/filter/LoginFilter.java
+++ src/main/java/com/web/filter/LoginFilter.java
@@ -2,8 +2,10 @@ package com.web.filter;
 
 import java.io.IOException;
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,10 +18,19 @@ public class LoginFilter extends HttpFilter {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         String remember = req.getParameter("remember");
-        res.getWriter().println(email);
-        res.getWriter().println(password);
-        res.getWriter().println(remember);
-        //chain.doFilter(req, res);
+        if(remember != null) {
+            Cookie cookie = new Cookie("email", email); // 建立 Cookie 物件
+            cookie.setMaxAge(30); // 存續時間
+            res.addCookie(cookie); // 加入到 response 物件
+        }
+        if(email.contains("admin")) {
+            chain.doFilter(req, res);
+        } else {
+            // 重導致 loginform.jsp
+            RequestDispatcher rd = getServletContext()
+                    .getRequestDispatcher("/JavaWeb0727/form/loginform.jsp");
+            rd.forward(req, res);
+        }
     }
     
 }
