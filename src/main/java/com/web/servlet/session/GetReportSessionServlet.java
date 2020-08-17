63d2a6950541009a7dd95e32ab5082eb19a230f4
teacher <teacher@192.168.2.14>
2020-08-17 20:43:27 +0800

    JavaWeb

--- src/main/java/com/web/servlet/session/GetReportSessionServlet.java
+++ src/main/java/com/web/servlet/session/GetReportSessionServlet.java
@@ -1,6 +1,24 @@
 package com.web.servlet.session;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class GetReportSessionServlet {
+@WebServlet("/servlet/session/getreport")
+public class GetReportSessionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        resp.getWriter().println(session);
+        if(session != null) {
+            resp.getWriter().println(session.getId());
+            resp.getWriter().println("Report data ...");
+        }
+    }
     
 }
