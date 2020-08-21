55a70f238bda1cdacf4802375db1f4ce45384f40
teacher <teacher@192.168.2.14>
2020-08-21 20:39:39 +0800

    JavaWeb

---
+++ src/main/java/com/bigdata/controller/BigDataServlet.java
@@ -0,0 +1,9 @@
+package com.bigdata.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/servlet/bigdata")
+public class BigDataServlet extends HttpServlet {
+    
+}
