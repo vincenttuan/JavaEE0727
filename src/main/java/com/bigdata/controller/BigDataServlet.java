6d86491df8d305330903dc7528d13c3d4eb62609
teacher <teacher@192.168.2.14>
2020-08-21 20:42:42 +0800

    JavaWeb

--- src/main/java/com/bigdata/controller/BigDataServlet.java
+++ src/main/java/com/bigdata/controller/BigDataServlet.java
@@ -1,9 +1,25 @@
 package com.bigdata.controller;
 
+import com.bigdata.model.Sales;
+import com.bigdata.model.SalesModel;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/servlet/bigdata")
 public class BigDataServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Sales[] saleses = SalesModel.getSaleses();
+        // 設定 request attribute
+        req.setAttribute("saleses", saleses);
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/form/bigdataform.jsp");
+        rd.forward(req, resp);
+    }
     
 }
