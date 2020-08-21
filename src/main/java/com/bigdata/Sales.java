8e9340899f5356c5494704ddd2e297c089743bb2
teacher <teacher@192.168.2.14>
2020-08-21 19:40:23 +0800

    JavaWeb

---
+++ src/main/java/com/bigdata/Sales.java
@@ -0,0 +1,20 @@
+package com.bigdata;
+
+public class Sales {
+
+    private String region;
+    private String country;
+    private String iemtype;
+    private String saleschannel;
+    private String orderpriority;
+    private String orderdate;
+    private int orderid;
+    private String shipdate;
+    private int unitssold;
+    private double unitprice;
+    private double unitcost;
+    private double totalrevenue;
+    private double totalcost;
+    private double totalprofit;
+    
+}
