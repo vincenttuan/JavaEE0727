5441e38d7d5a0157b71130180cfe0d807f36b651
teacher <teacher@192.168.2.14>
2020-08-21 20:00:24 +0800

    JavaWeb

--- src/main/java/com/bigdata/model/SalesModel.java
+++ src/main/java/com/bigdata/model/SalesModel.java
@@ -1,5 +1,6 @@
 package com.bigdata.model;
 
+import com.google.gson.Gson;
 import java.io.File;
 import java.util.Scanner;
 
@@ -13,9 +14,14 @@ public class SalesModel {
             String path = "src\\main\\java\\com\\bigdata\\model\\csvjson.json";
             File file = new File(path);
             String json = new Scanner(file).useDelimiter("\\A").next();
-            
+            saleses = new Gson().fromJson(json, Sales[].class);
         } catch (Exception e) {
         }
-
     }
+
+    public static Sales[] getSaleses() {
+        return saleses;
+    }
+    
+    
 }
