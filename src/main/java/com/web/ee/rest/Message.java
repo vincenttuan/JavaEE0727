ecc195d5596b78bd492567db35b7d2f2f3959385
teacher <teacher@192.168.2.14>
2020-08-24 20:57:25 +0800

    JavaEE

---
+++ src/main/java/com/web/ee/rest/Message.java
@@ -0,0 +1,31 @@
+package com.web.ee.rest;
+
+public class Message {
+    private String message;
+    private int code;
+
+    public Message() {
+    }
+
+    public Message(String message, int code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+    
+}
--- src/main/java/com/web/ee/rest/UserService.java
+++ src/main/java/com/web/ee/rest/UserService.java
@@ -3,7 +3,9 @@ package com.web.ee.rest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -30,5 +32,14 @@ public class UserService {
                 .filter(u -> u.getId() == id)
                 .findFirst();
         return optUser.isPresent()?optUser.get():null;
+    }
+    
+    @Path("{id}")
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Message addUser(User user) {
+        users.add(user);
+        return new Message("新增成功", 200);
     }
 }
