5596db4ff48d3a2dbbbc67514da873aad67311f8
teacher <teacher@192.168.2.14>
2020-08-24 21:47:38 +0800

    JavaEE

--- src/main/java/com/web/ee/rest/UserService.java
+++ src/main/java/com/web/ee/rest/UserService.java
@@ -12,6 +12,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("user")
@@ -74,8 +75,8 @@ public class UserService {
     // http://localhost:8080/JavaEE0727/rest/user/1
     @Path("{id}")
     @DELETE
-    @Produces("application/json")
-    @Consumes("application/json")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public Response deleteUser(@PathParam("id") int id) {
         boolean check = users.removeIf(u -> u.getId() == id);
         Response resp = Response.ok()
