86e8bcc219c63b6af653d02f18fb1cd25e84863f
teacher <teacher@192.168.2.14>
2020-09-04 19:49:01 +0800

    JavaEE

--- src/main/java/com/web/ee/jdbc/DBConnectionManager.java
+++ src/main/java/com/web/ee/jdbc/DBConnectionManager.java
@@ -4,13 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnectionManager {
+    private Connection conn = null;
     public Connection getConnection(String jdbcUrl, String username, String password) {
-        Connection conn = null;
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             conn = DriverManager.getConnection(jdbcUrl, username, password);
         } catch (Exception e) {
+            e.printStackTrace();
         }
         return conn;
+    }
+    public void close() {
+        if (conn == null) return;
+        try {
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
