18b2b052e79048aba516b0dbaf0a6e71d2f8be34
teacher <teacher@192.168.2.14>
2020-09-07 19:54:51 +0800

    JavaEE

--- src/main/java/com/web/ee/jpa/servlet/AddStudent.java
+++ src/main/java/com/web/ee/jpa/servlet/AddStudent.java
@@ -1,8 +1,11 @@
 package com.web.ee.jpa.servlet;
 
+import com.web.ee.jpa.entity.Student;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,6 +25,19 @@ public class AddStudent extends HttpServlet{
         // 取得 EntityManager
         // myjpa 是 unit name, 請參考 persistence.xml 中的設定
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("myjpa");
+        EntityManager em = emf.createEntityManager();
+        // 建立 Student 物件/Entity
+        Student student = new Student();
+        student.setName("John");
+        student.setAge(18);
+        // 將 Student 物件/Entity 映射到資料庫, 也就是新增一筆紀錄
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(student);
+        et.commit();
+        em.close();
+        emf.close();
+        out.print("Add OK");
     }
     
 }
