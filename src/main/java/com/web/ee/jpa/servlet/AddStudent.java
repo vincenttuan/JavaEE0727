e4927e4ae8eea654988b5ea55c90f7f82e47f2b8
teacher <teacher@192.168.2.14>
2020-09-07 20:04:02 +0800

    JavaEE

--- src/main/java/com/web/ee/jpa/servlet/AddStudent.java
+++ src/main/java/com/web/ee/jpa/servlet/AddStudent.java
@@ -1,8 +1,10 @@
 package com.web.ee.jpa.servlet;
 
+import com.github.javafaker.Faker;
 import com.web.ee.jpa.entity.Student;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -28,8 +30,8 @@ public class AddStudent extends HttpServlet{
         EntityManager em = emf.createEntityManager();
         // 建立 Student 物件/Entity
         Student student = new Student();
-        student.setName("John");
-        student.setAge(18);
+        student.setName(new Faker().pokemon().name());
+        student.setAge(new Random().nextInt(10) + 18);
         // 將 Student 物件/Entity 映射到資料庫, 也就是新增一筆紀錄
         EntityTransaction et = em.getTransaction();
         et.begin();
