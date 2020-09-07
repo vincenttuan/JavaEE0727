25020eddb3678b786b2844a12ed873fc7a50b243
teacher <teacher@192.168.2.14>
2020-09-07 19:38:35 +0800

    JavaEE

---
+++ src/main/java/com/web/ee/jpa/servlet/AddStudent.java
@@ -0,0 +1,27 @@
+package com.web.ee.jpa.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/jpa/student/add")
+public class AddStudent extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        // 取得 EntityManager
+        // myjpa 是 unit name, 請參考 persistence.xml 中的設定
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("myjpa");
+    }
+    
+}
