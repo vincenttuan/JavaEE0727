5b2b3fbb4de9cdc39c3bb1497339d91e7b3e1bec
teacher <teacher@192.168.2.14>
2020-09-11 19:22:08 +0800

    JavaEE

--- src/main/java/com/web/ee/async/LongJob.java
+++ src/main/java/com/web/ee/async/LongJob.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.web.ee.async;
 
-/**
- *
- * @author teacher
- */
-public class LongJob {
+import javax.servlet.AsyncContext;
+
+public class LongJob implements Runnable {
+    private AsyncContext ctx;
+
+    public LongJob(AsyncContext ctx) {
+        this.ctx = ctx;
+    }
+    
+    @Override
+    public void run() {
+        
+    }
     
 }
--- src/main/java/com/web/ee/async/LongJobServlet.java
+++ src/main/java/com/web/ee/async/LongJobServlet.java
@@ -1,6 +1,9 @@
 package com.web.ee.async;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +18,15 @@ public class LongJobServlet extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print("進入 Servlet 時間: " + new Date() + "<p />");
+        out.flush();
+        // 商業邏輯
+        AsyncContext ctx = req.getAsyncContext();
+        LongJob job = new LongJob(ctx); // 建立 LongJob
+        new Thread(job).start(); // 使用一條執行緒去執行 LongJob
+        
+        out.print("離開 Servlet 時間: " + new Date() + "<p />");
         
     }
     
