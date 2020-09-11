d97052715abd8b732c001e86d4c4200c3fc8e110
teacher <teacher@192.168.2.14>
2020-09-11 19:14:47 +0800

    JavaEE

---
+++ src/main/java/com/web/ee/async/LongJobServlet.java
@@ -0,0 +1,21 @@
+package com.web.ee.async;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/async/longjob", asyncSupported = true)
+public class LongJobServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
+        
+    }
+    
+}
