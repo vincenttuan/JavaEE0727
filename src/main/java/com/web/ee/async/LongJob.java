c4d239d5a2a842f10b8169924ebc8bf008fcbac4
teacher <teacher@192.168.2.14>
2020-09-11 19:25:32 +0800

    JavaEE

--- src/main/java/com/web/ee/async/LongJob.java
+++ src/main/java/com/web/ee/async/LongJob.java
@@ -1,5 +1,7 @@
 package com.web.ee.async;
 
+import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.AsyncContext;
 
 public class LongJob implements Runnable {
@@ -11,7 +13,14 @@ public class LongJob implements Runnable {
     
     @Override
     public void run() {
-        
+        try {
+            Thread.sleep(10_000); // 停 10 秒
+            PrintWriter out = ctx.getResponse().getWriter();
+            out.print("LongJob 任務完成, 時間:" + new Date() + "<p/>");
+            out.flush();
+            ctx.complete(); // 非同步的工作完成
+        } catch (Exception e) {
+        }
     }
     
 }
