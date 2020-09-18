c4e1435ec7becdda8cdb19aabcc0023df31f081c
teacher <teacher@192.168.2.14>
2020-09-18 21:06:56 +0800

    JavaEE

---
+++ src/main/java/com/web/ee/tag/HelloSimpleTag.java
@@ -0,0 +1,22 @@
+package com.web.ee.tag;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class HelloSimpleTag extends SimpleTagSupport{
+    private String msg;
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+    @Override
+    public void doTag() throws JspException, IOException {
+        JspWriter out = this.getJspContext().getOut();
+        out.print("我是一個很簡單的 Tag, msg: " + msg);
+                
+    }
+    
+}
