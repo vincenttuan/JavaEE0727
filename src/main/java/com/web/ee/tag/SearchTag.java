baa5c891eb704cc471727ea42943ceb1d8b6c75c
teacher <teacher@192.168.2.14>
2020-09-18 19:41:30 +0800

    JavaEE

--- src/main/java/com/web/ee/tag/SearchTag.java
+++ src/main/java/com/web/ee/tag/SearchTag.java
@@ -11,7 +11,7 @@ public class SearchTag implements BodyTag {
     private PageContext pageContext;
     private Tag parentTag;
     private BodyContent bodyContent;
-    private String key;
+    private String key, key2;
     @Override
     public void setPageContext(PageContext pc) {
         pageContext = pc;
@@ -31,6 +31,10 @@ public class SearchTag implements BodyTag {
         this.key = key;
     }
 
+    public void setKey2(String key2) {
+        this.key2 = key2;
+    }
+
     @Override
     public void doInitBody() throws JspException {
         
@@ -41,6 +45,10 @@ public class SearchTag implements BodyTag {
         String content = bodyContent.getString();
         String new_content = content.replaceAll(key, 
                 String.format("<font color=red><b>%s</b></font>", key));
+        if(key2 != null) {
+            new_content = new_content.replaceAll(key2, 
+                String.format("<font color=blue><b>%s</b></font>", key2));
+        }
         JspWriter out = bodyContent.getEnclosingWriter();
         try {
             out.print(new_content);
@@ -57,7 +65,11 @@ public class SearchTag implements BodyTag {
     public String getKey() {
         return key;
     }
-
+    
+    public String getKey2() {
+        return key2;
+    }
+    
     @Override
     public int doStartTag() throws JspException {
         return BodyTag.EVAL_BODY_BUFFERED;
