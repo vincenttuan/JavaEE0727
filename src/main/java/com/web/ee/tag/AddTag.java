efa837d8215b41c7ff2f528d7e252b866ae6e5f2
teacher <teacher@192.168.2.14>
2020-09-18 21:19:06 +0800

    JavaEE

---
+++ src/main/java/com/web/ee/tag/AddTag.java
@@ -0,0 +1,24 @@
+package com.web.ee.tag;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.DynamicAttributes;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class AddTag extends SimpleTagSupport implements DynamicAttributes{
+    private Map<String, Integer> map = new LinkedHashMap<>();
+    @Override
+    public void doTag() throws JspException, IOException {
+        JspWriter out = getJspContext().getOut();
+        out.print(map);
+    }
+    
+    @Override
+    public void setDynamicAttribute(String uri, String localName, Object value) throws JspException {
+        map.put(localName, Integer.parseInt(value.toString()));
+    }
+    
+}
